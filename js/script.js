3c5ae2cd0f4d5c1d14fbf5aeea4bd5fd7a08cfc9
Kiril <kiril.chernysh@gmail.com>
2017-09-09 14:19:41 +0300

    beta3.9

--- js/script.js
+++ js/script.js
@@ -82,18 +82,18 @@
       }
     });
   //MOBILE_MENU
-$(document).delegate('.open', 'click', function(event){
-			$(this).addClass('oppenned');
-			event.stopPropagation();
-		})
-		$(document).delegate('body', 'click', function(event) {
-			$('.open').removeClass('oppenned');
-		})
-		$(document).delegate('.cls', 'click', function(event){
-			$('.open').removeClass('oppenned');
-			event.stopPropagation();
-		});
-
+  :
+  $(document).delegate('.open', 'click', function(event){
+        $(this).addClass('oppenned');
+        event.stopPropagation();
+      })
+      $(document).delegate('.oppenned', 'click', function(event) {
+        $('.oppenned').removeClass('oppenned');
+      })
+      $(document).delegate('.cls', 'click', function(event){
+        $('.open').removeClass('oppenned');
+        event.stopPropagation();
+      });
 
 
 
