4b07767f64ff0b0f151e875d206028363810fd7b
Kiril <kiril.chernysh@gmail.com>
2017-09-09 14:47:05 +0300

    beta4.1

--- js/script.js
+++ js/script.js
@@ -93,7 +93,19 @@
         $('.open').removeClass('oppenned');
         event.stopPropagation();
       });
+  //SCROll  to anchor
+  $('a[href^="#"]').bind('click.smoothscroll',function (e) {
+ e.preventDefault();
 
+var target = this.hash,
+ $target = $(target);
+
+$('html, body').stop().animate({
+ 'scrollTop': $target.offset().top
+ }, 500, 'swing', function () {
+ window.location.hash = target;
+ });
+ });
 
 
   });
